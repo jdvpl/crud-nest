012f93f368442480e51011a9c979d570cbfb066f
juan daniel suarez amado <62775732+jdvpl@users.noreply.github.com>
2023-02-19 13:42:05 -0500

    connecting to pg raiway service

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,22 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { Todo } from './todos/entities/todo.entity';
 import { TodosModule } from './todos/todos.module';
 
 @Module({
-  imports: [TodosModule],
+  imports: [TypeOrmModule.forRoot({
+    type: 'postgres',
+    host: 'containers-us-west-67.railway.app',
+    port: 7726,
+    username: 'postgres',
+    password: 'hbMRFgSVNiFrAFFDx9XL',
+    database: 'railway',
+    entities: [Todo],
+    // synchronize: true
+  }), TodosModule],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule { }
--- src/todos/entities/todo.entity.ts
+++ src/todos/entities/todo.entity.ts
@@ -1,1 +1,14 @@
-export class Todo {}
+import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
+
+@Entity()
+export class Todo {
+  @PrimaryGeneratedColumn()
+  id?: number;
+
+  @Column()
+  title: string;
+
+  @Column()
+  completed: boolean;
+}
+
