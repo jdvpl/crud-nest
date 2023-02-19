073097c7e960665aff46a374271aaae81c740c6d
juan daniel suarez amado <62775732+jdvpl@users.noreply.github.com>
2023-02-19 17:10:00 -0500

    finished all endpoints

--- src/todos/todos.service.ts
+++ src/todos/todos.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
-import { Repository } from 'typeorm';
+import { DeleteResult, Repository } from 'typeorm';
 import { CreateTodoDto } from './dto/create-todo.dto';
 import { UpdateTodoDto } from './dto/update-todo.dto';
 import { Todo } from './entities/todo.entity';
@@ -13,23 +13,29 @@ export class TodosService {
   ) {
 
   }
-  create() {
-    return 'This action adds a new todo';
+  async create(createTodoDto: CreateTodoDto): Promise<Todo> {
+    return this.todoRepository.save(createTodoDto);
   }
 
-  findAll() {
-    return `This action returns all todos`;
+  async findAll(): Promise<Todo[]> {
+    return this.todoRepository.find();
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} todo`;
+  async findOne(id: number): Promise<Todo> {
+    return await this.todoRepository.findOneBy({ id });
   }
 
-  update(id: number, updateTodoDto: UpdateTodoDto) {
-    return `This action updates a #${id} todo`;
+  async update(id: number, updateTodoDto: UpdateTodoDto): Promise<Todo> {
+
+    try {
+      await this.todoRepository.update(id, updateTodoDto);
+      return this.todoRepository.findOneBy({ id });
+    } catch (error) {
+      return error;
+    }
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} todo`;
+  async remove(id: number): Promise<DeleteResult> {
+    return this.todoRepository.delete(id);
   }
 }
