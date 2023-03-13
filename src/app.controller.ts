9921eda86bad328f5a5cb9b84c4bad094e58af0b
juan daniel suarez amado <62775732+jdvpl@users.noreply.github.com>
2023-03-13 15:35:13 -0500

    sending pdf by email

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -7,7 +7,12 @@ export class AppController {
 
   @Post("pdf")
   async downloadPDF(@Body() body): Promise<{ doc: string }> {
+    console.log({ body })
     return await this.appService.generatePDF();
+  }
 
+  @Post("email")
+  async sendEmail(@Body() body): Promise<{ email: string }> {
+    return await this.appService.sendEmail()
   }
 }
--- src/app.service.ts
+++ src/app.service.ts
@@ -1,6 +1,11 @@
 import { Injectable } from '@nestjs/common';
 import PDFDocumentWithTables from 'pdfkit-table';
 import axios from 'axios';
+import * as nodemailer from 'nodemailer';
+import * as smtpTransport from 'nodemailer-smtp-transport';
+import { ConfigService } from '@nestjs/config';
+import { join } from 'path';
+import * as fs from 'fs';
 
 const city = "Bogota"
 const date = "19 de septiembre de 2023"
@@ -10,9 +15,9 @@ const maxAmount = '$67.986.824 pesos';
 const termFinance = '24 meses';
 const amotizationype = "Cuota fija en pesos";
 
-const basePath = "https://aziddevstacmrtggse2000.blob.core.windows.net/aziddevstcoimge2000/approvals";
+const basePathURL = "https://aziddevstacmrtggse2000.blob.core.windows.net/aziddevstcoimge2000/approvals";
 
-async function fetchImage(imagePath: string) {
+async function fetchImage(imagePath: string, basePath: string = basePathURL) {
   const image = await axios
     .get(basePath + imagePath, {
       responseType: 'arraybuffer'
@@ -21,7 +26,7 @@ async function fetchImage(imagePath: string) {
 }
 
 async function getFont(url: string) {
-  const response = await axios.get(basePath + url, {
+  const response = await axios.get(basePathURL + url, {
     responseType: 'arraybuffer',
   });
   const buffer = Buffer.from(response.data);
@@ -29,7 +34,10 @@ async function getFont(url: string) {
 }
 @Injectable()
 export class AppService {
-  async generatePDF(): Promise<{ doc: string }> {
+  constructor(
+    private readonly configService: ConfigService,
+  ) { }
+  async generatePDF(): Promise<{ doc: string, name: string }> {
     const pdfBuffer: Buffer = await new Promise(async resolve => {
       const doc = new PDFDocumentWithTables(
         {
@@ -110,6 +118,54 @@ export class AppService {
       })
       doc.end()
     })
-    return { doc: pdfBuffer.toString("base64") };
+    return { doc: pdfBuffer.toString("base64"), name: `carta_aprobacion_${city}` };
+  }
+
+  async sendEmail(): Promise<{ email: string }> {
+    const mailer = nodemailer.createTransport(
+      smtpTransport({
+        host: 'sandbox.smtp.mailtrap.io',
+        port: 2525,
+        auth: {
+          user: "4e5c3e15a92ad4",
+          pass: "47883064178c74"
+        }
+      })
+    );
+
+    type gender = "F" | "M";
+
+    const basePathImg = this.configService.get("BASEPATH_EMAIL_IMGS");
+    const urlRobotoLight = 'https://fonts.googleapis.com/css2?family=Roboto:wght@300&display=swap';
+    const urlRobotoMedium = 'https://fonts.googleapis.com/css2?family=RobotoBold:wght@500&display=swap';
+    const gender: gender = "F";
+    const bannerGender = gender === "F" ? `${basePathImg}femaleBanner.svg` : `${basePathImg}maleBanner.svg`;
+    const appreciated = gender === "F" ? 'Apreciada' : 'Apreciado';
+    const nameUser = "Luz Agudelo ";
+    const price = '$ 42.319.075';
+    const term = "5 años";
+
+    const pdfPath = join(process.cwd(), "assets/img/bcs_carta_preaprobado_CC17168784.pdf")
+    const pdf = fs.readFileSync(pdfPath).toString("base64");
+    const mailOptions = {
+      from: 'juanda554242@gmail.com',
+      to: 'juanda554242@gmail.com',
+      subject: 'Carta de preaprobacion',
+      html: `<html lang="es"><head><meta charset="UTF-8"><meta http-equiv="X-UA-Compatible" content="IE=edge"><meta name="viewport" content="width=device-width, initial-scale=1.0"><style>@import url("${urlRobotoLight}"); @import url("${urlRobotoMedium}"); .white{ color: white;} .textCenter{ text-align: center;} .py2{ padding-top: 1rem; padding-bottom: 1rem;} *{ margin: 0; padding: 0;} .fRoboto{ font-family: 'Roboto', sans-serif;} .w90{ width: 85%; margin: auto;} .p0m0{ display: flex; flex-direction: column;} .ftBlue{ color: #005DA2;} .my1{ margin-top: 1rem; max-width: 1rem;} .mt3{ margin-top: 2rem;} .ftBold{ font-family: 'RobotoBold', sans-serif; font-weight: 700;} .mt1{ margin-top: 0.5rem;} .justify{ display: flex; justify-content: flex-start;} .mauto{ margin: auto;} .w10{ width: 5%;} .signImg{ margin-top: 2.5rem; margin-right: 1rem; margin-left: 1rem;} .bgFeeImg{ padding: 0; margin: 0; background-image: url('${basePathImg}feeBg.svg'); background-repeat: no-repeat; background-position: center; background-size: cover; width: 255px; height: 255px;} .blueAjunts{ padding-top: 33%; font-size: 12px; font-weight: 700; width: 60%; margin: auto; color: #0072C8;} .txtTerm{ font-size: 16px; padding-top: 0.4rem;} .fs27{ font-size: 24px;} .ftNormal{ letter-spacing: normal;} .fcApre{ color: #00253D;} .colorPre{ color: #496374;} .pt2{ padding-top: 2rem;} .my5{ margin-top: 3rem; margin-bottom: 3rem;} .mb5{ margin-bottom: 3rem;} .f20{ font-size: 20px;} .ftxtNormal{ text-transform: none;} .mx1{ margin: 0 3px 0 3px;} .bgWhite{ background-color: white;} .dflex{ display: flex;} .w60{ width: 40%; margin-left: 3%;} .w30{ width: 70%; display: flex; justify-content: end;} .w100{ width: 100%;} .pt4{ padding-top: 2rem;} @media (max-width:400px){ .dflex{ flex-direction: column; justify-content: center; place-items: center;} .w60{ width: 100%;} .w30{ width: 100%;} .w10{ display: none;}} </style></head><body><main class="w100 bgWhite"><header><div><a href="https://www.bancocajasocial.com"><img src="${basePathImg}headerBannerClick.svg" alt="" width="100%" class="p0m0"></a></div><div class="p0m0"><img src="${bannerGender}" alt="" class="p0m0" width="100%"></div></header><div class="w90 bgWhite"><p class="my1 fRoboto fcApre">${appreciated}</p><p class="ftBold mt1 f20">${nameUser}</p></div><div class="dflex w100 mt3"><div class="w10"><div class="signImg"><img src="${basePathImg}signing.svg" alt="" height="70%"></div></div><div class="w60"><img src="${basePathImg}bodyText.svg" alt="" width="100%"></div><div class="w30"><div class="mauto my5"><div class="bgFeeImg textCenter"><p class="fRoboto colorPre pt4">Monto preaprobado</p><p class="ftBlue fs27 mt1 ftBold">${price}</p><p class="txtTerm colorPre fRoboto">Plazo | <span class="ftBlue ftNormal ftxtNormal">${term}</span></p><p class="blueAjunts ftBold">Adjunto encontrará la carta de preaprobación. </p></div></div></div></div><footer><img src="${basePathImg}footer.svg" alt="" width="100%"></footer></main></body></html>`,
+      attachments: [{
+        filename: 'carta_preaprobacion.pdf',
+        content: pdf
+      }]
+    };
+
+    mailer.sendMail(mailOptions, (error, info) => {
+      if (error) {
+        console.log(error);
+      } else {
+        console.log('Correo electrónico enviado: ' + info.response);
+      }
+    });
+
+    return { email: "juanda54242@gmail.com" }
   }
 }
